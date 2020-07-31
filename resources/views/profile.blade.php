a7a0b1f9a737731610a63d50497fca5625b0b940
thanakorn26443 <thanakorn26443@gmail.com>
2020-07-31 12:51:26 +0700

    profile

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,4 @@
+<h1>ธนากร ชุลี</h1>
+<a href="#" class="thumbnail">
+ <img src="https://scontent.fbkk10-1.fna.fbcdn.net/v/t1.0-9/45723556_1080239862154430_1015740962137178112_o.jpg?_nc_cat=110&_nc_sid=e3f864&_nc_eui2=AeHJpVD3LfTmtLFC-dHFxkC8IW9tEBM9guYhb20QEz2C5st0Hx3gdoAdJ7CN2M2iKuqDdlJUIRQnDT7rBH5H22Tl&_nc_ohc=YIpRZ34o1mUAX-uKDub&_nc_ht=scontent.fbkk10-1.fna&oh=83d181e22fe91a3ac2935e8338c3a42e&oe=5F49C6BE" height = "150" />
+</a>
